2a25d34fb6e44de21289278a816f629061d5efbc
Albar <mubarrid.syahrir@xtremax.com>
2020-03-04 13:27:05 +0700

    fix auto Id Guid generation

--- src/Entity/Course.cs
+++ src/Entity/Course.cs
@@ -5,7 +5,7 @@ namespace KnowledgeShare.Entity
 {
     public class Course
     {
-        public string Id { get; } = new Guid().ToString();
+        public string Id { get; } = Guid.NewGuid().ToString();
         public ICourseUser Author { get; set; }
         public string Title { get; set; }
         public ICourseUser Speaker { get; set; }
