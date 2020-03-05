c8c876d92ffb60ebc6f221b9cd9cc9194a8c3fe9
Albar <mubarrid.syahrir@xtremax.com>
2020-03-05 13:55:25 +0700

    fix inaccurate property name

--- src/Manager/Validation/ValidationErrorsBag.cs
+++ src/Manager/Validation/ValidationErrorsBag.cs
@@ -4,17 +4,17 @@ namespace KnowledgeShare.Manager.Validation
 {
     public class ValidationErrorsBag
     {
-        public List<string> ErrorsMessage { get; } = new List<string>();
-        public int Count => ErrorsMessage.Count;
+        public List<string> ErrorMessages { get; } = new List<string>();
+        public int Count => ErrorMessages.Count;
 
         public void Append(ValidationErrorsBag errorsBag)
         {
-            ErrorsMessage.AddRange(errorsBag.ErrorsMessage);
+            ErrorMessages.AddRange(errorsBag.ErrorMessages);
         }
 
         public void Add(string errorMessage)
         {
-            ErrorsMessage.Add(errorMessage);
+            ErrorMessages.Add(errorMessage);
         }
     }
 }
