0bc4575af97668af56435d1f15eb5542bb11466c
Albar <mubarrid.syahrir@xtremax.com>
2020-03-05 14:00:56 +0700

    add id to attendee

--- src/Entity/Invitee.cs
+++ src/Entity/Invitee.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace KnowledgeShare.Entity
 {
     public class Invitee
     {
+        public string Id { get; } = Guid.NewGuid().ToString();
         public ICourseUser User { get; set; }
         public Course Course { get; set; }
     }
