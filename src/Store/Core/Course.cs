818a56a38b2c4a58c35a2cbc0b541978f1d4d83b
Albar <mubarrid.albar@outlook.com>
2020-03-06 20:44:09 +0700

    extract Location conversions to methods

--- src/Store/Core/Course.cs
+++ src/Store/Core/Course.cs
@@ -7,49 +7,54 @@ namespace KnowledgeShare.Store.Core
     public class Course
     {
         public string Id { get; } = Guid.NewGuid().ToString();
-        public virtual CourseUser Author { get; set; }
+        public CourseUser Author { get; set; }
         public string Title { get; set; }
-        public virtual CourseUser Speaker { get; set; }
+        public CourseUser Speaker { get; set; }
         public string Description { get; set; }
         public CourseVisibility Visibility { get; set; }
-        public virtual ILocation Location
+        public ILocation Location
         {
-            get
-            {
-                if (_locationCache is { })
-                {
-                    return _locationCache;
-                }
-                if (_location == null)
-                {
-                    return null;
-                }
-
-                return _locationCache ??= (ILocation)JsonConvert.DeserializeObject(
-                    _location, Type.GetType(_locationType));
-            }
-            set
-            {
-                _locationCache = value;
-
-                if (value == null)
-                {
-                    _locationType = nameof(ILocation);
-                    _locationType = null;
-                    return;
-                }
-
-                _locationType = value.GetType().FullName;
-                _location = JsonConvert.SerializeObject(value);
-            }
+            get => GetLocation();
+            set => SetLocation(value);
         }
-        public virtual List<Session> Sessions { get; set; } = new List<Session>();
+        public List<Session> Sessions { get; set; } = new List<Session>();
 
-        public virtual List<Registrant> Registrants { get; } = new List<Registrant>();
-        public virtual List<Feedback> Feedbacks { get; } = new List<Feedback>();
+        public List<Registrant> Registrants { get; } = new List<Registrant>();
+        public List<Feedback> Feedbacks { get; } = new List<Feedback>();
 
         private string _location = null;
-        private string _locationType = nameof(ILocation);
+        private string _locationType = typeof(ILocation).AssemblyQualifiedName;
         private ILocation _locationCache = null;
+
+        private ILocation GetLocation()
+        {
+            if (_locationCache is { })
+            {
+                return _locationCache;
+            }
+            if (_location == null)
+            {
+                _locationType = typeof(ILocation).AssemblyQualifiedName;
+                return null;
+            }
+
+            return _locationCache ??= (ILocation)JsonConvert.DeserializeObject(
+                _location, Type.GetType(_locationType));
+        }
+
+        private void SetLocation(ILocation location)
+        {
+            _locationCache = location;
+
+            if (location == null)
+            {
+                _locationType = typeof(ILocation).AssemblyQualifiedName;
+                _location = null;
+                return;
+            }
+
+            _locationType = location.GetType().AssemblyQualifiedName;
+            _location = JsonConvert.SerializeObject(location);
+        }
     }
 }
