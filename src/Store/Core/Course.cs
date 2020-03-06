4bc5ed7457b42dc74e1966b9cf2f573f3d8a4cd0
Albar <mubarrid.syahrir@xtremax.com>
2020-03-06 13:36:11 +0700

    implement Course Location Conversion

--- src/Store/Core/Course.cs
+++ src/Store/Core/Course.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace KnowledgeShare.Store.Core
 {
@@ -11,10 +12,44 @@ namespace KnowledgeShare.Store.Core
         public virtual CourseUser Speaker { get; set; }
         public string Description { get; set; }
         public CourseVisibility Visibility { get; set; }
-        public virtual ILocation Location { get; set; }
+        public virtual ILocation Location
+        {
+            get
+            {
+                if (_locationCache is { })
+                {
+                    return _locationCache;
+                }
+                if (_location == null)
+                {
+                    return null;
+                }
+
+                return _locationCache ??= (ILocation)JsonConvert.DeserializeObject(
+                    _location, Type.GetType(_locationType));
+            }
+            set
+            {
+                _locationCache = value;
+
+                if (value == null)
+                {
+                    _locationType = nameof(ILocation);
+                    _locationType = null;
+                    return;
+                }
+
+                _locationType = value.GetType().FullName;
+                _location = JsonConvert.SerializeObject(value);
+            }
+        }
         public virtual List<Session> Sessions { get; set; } = new List<Session>();
 
         public virtual List<Registrant> Registrants { get; } = new List<Registrant>();
         public virtual List<Feedback> Feedbacks { get; } = new List<Feedback>();
+
+        private string _location = null;
+        private string _locationType = nameof(ILocation);
+        private ILocation _locationCache = null;
     }
 }
