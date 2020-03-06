2de05557825bef21465d52bd171e10706ed8c57c
Albar <mubarrid.syahrir@xtremax.com>
2020-03-06 17:52:43 +0700

    cleanup client-app

--- src/Server/client-app/src/main.ts
+++ src/Server/client-app/src/main.ts
@@ -1,9 +1,18 @@
 import Vue from 'vue';
+import axios from 'axios';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$client = axios.create({
+  baseURL: `${process.env.baseURL}/api/`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 new Vue({
   router,
